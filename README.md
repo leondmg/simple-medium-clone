f4ba4ef3876555ce08bc3ee7aa357b0e13149774
Dmitry Leontev <dmitriy1leontev@gmail.com>
2022-12-20 08:03:27 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# README
+
+Simple medium clone
+
+* Ruby v3.1.3
+
+* Rails v7.0.4
+
+## Gems
+
+* rubocop
+* slim
+* pg
+* rails_best_practices
---
+++ test/channels/application_cable/connection_test.rb
@@ -0,0 +1,13 @@
+require 'test_helper'
+
+# rubocop:disable Style/ClassAndModuleChildren
+class ApplicationCable::ConnectionTest < ActionCable::Connection::TestCase
+  # test "connects with cookies" do
+  #   cookies.signed[:user_id] = 42
+  #
+  #   connect
+  #
+  #   assert_equal connection.user_id, "42"
+  # end
+end
+# rubocop:enable Style/ClassAndModuleChildren
